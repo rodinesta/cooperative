7073c0e7f8edceb5c0ea2c741d814bcb8b15c96a
Anton <gr602_seaan>
2023-06-26 19:19:22 +0700

    v0.4

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import {Container} from "react-bootstrap";
+import React, {useContext, useEffect} from 'react';
+import {Container, Row} from "react-bootstrap";
 import Slider from "../components/Slider";
+import ReviewItem from "../components/ReviewItem";
+import {Context} from "../index";
+import ReviewList from "../components/ReviewList";
+import {receiveReviews} from "../http/ReviewAPI";
 
 const Home = () => {
+    const {review} = useContext(Context)
+
+    useEffect(() => {
+        receiveReviews().then(data => {
+            review.setReview(data)
+        })
+    }, [review])
+
     return (
         <div style={{background: "#FFFFFF"}}>
             <div className='image'>
@@ -16,6 +28,12 @@ const Home = () => {
                 <text>Мы занимаемся помощью в обслуживании дач, находящихся в дачном кооперативе “Кужлевка”. На данном сайте вы найдете информацию о правлении, графике работы, а так же контактные данные. </text>
                 <h2>Фотографии</h2>
                 <Slider/>
+                <h2>Отзывы</h2>
+                <Row className="d-flex">
+                    {review.review?.slice(0, 2).map(review =>
+                        <ReviewItem key={review.id} review={review}/>
+                    )}
+                </Row>
                 <h2>Контактные данные</h2>
                 <text>+7 (777) 777-77-77 Россия, Томск, 123456, р-н Кужлевка СТ</text>
             </Container>
